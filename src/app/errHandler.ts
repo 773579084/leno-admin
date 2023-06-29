1e05ca85f804fb5366f17bdced3d7db539264485
zhaowenchao <773579084@qq.com>
2023-06-29 16:37:10 +0800

    ci:message 消息提示优化

--- src/app/errHandler.ts
+++ src/app/errHandler.ts
@@ -5,6 +5,12 @@ export default function errHandlerFn(err: any, ctx: Context) {
   // 失败日志采集
   writeLog('1', ctx, err)
 
+  /** code 代表含义
+   * 400 => 表示前端传参可能出现错误
+   * 401 => 权限过期
+   * 403 => 无访问权限
+   * 500 => 服务器拒绝请求
+   */
   let status = 500
   switch (err.code) {
     case '400':
