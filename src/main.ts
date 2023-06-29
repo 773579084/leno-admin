f2942e8cd5959cac55b320443ebdac2c4210e9d8
zhaowenchao <773579084@qq.com>
2023-06-29 14:14:52 +0800

    feat:服务器地址打印变为蓝色

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,5 @@ import app from '@/app/index'
 const { APP_PORT, APP_HOST, APP_HTTP } = process.env
 
 app.listen(APP_PORT, () => {
-  console.log(`服务器启动成功,running ${APP_HTTP}://${APP_HOST}:${APP_PORT}`)
+  console.log('\x1b[34m%s\x1b[0m', `服务器启动成功,running ${APP_HTTP}://${APP_HOST}:${APP_PORT}`)
 })
