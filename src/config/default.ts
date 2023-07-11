5c851c13476784bd884627346edec9dcd68163b0
zhaowenchao <773579084@qq.com>
2023-07-11 14:53:14 +0800

    fix:修复tsc转译js后无法启动的问题

--- src/config/default.ts
+++ src/config/default.ts
@@ -1,4 +1,5 @@
 export default () => {
+  // 重要：需要 cross-env 注入变量启动，才能够全局使用，直接用node启动文件无法获取变量
   if (process.env.NODE_ENV === 'development') {
     return {
       // 服务器运行地址及端口
@@ -20,14 +21,14 @@ export default () => {
       REDIS_PASSWORD: 'admin123',
       REDIS_DB: 0
     }
-  } else {
+  } else if (process.env.NODE_ENV === 'production') {
     return {
       // 服务器运行地址及端口
-      APP_HOST: '192.168.59.209',
+      APP_HOST: 'localhost',
       APP_PORT: 9000,
       APP_HTTP: 'http',
       // mysql
-      MYSQL_HOST: '192.168.59.209',
+      MYSQL_HOST: 'localhost',
       MYSQL_PORT: 3306,
       MYSQL_USER: 'root',
       MYSQL_PWD: 'admin123',
@@ -37,7 +38,7 @@ export default () => {
       JWT_REFRESH_SECRET: 'refreshToken',
       // redis
       REDIS_PORT: 6379,
-      REDIS_HOST: '192.168.59.209',
+      REDIS_HOST: 'localhost',
       REDIS_PASSWORD: 'admin123',
       REDIS_DB: 0
     }
--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import env from '@/config/default'
 
 const { APP_PORT, APP_HOST, APP_HTTP } = env()
 
-app.listen(env().APP_PORT, () => {
+app.listen(APP_PORT, () => {
   console.log(
     `服务器启动成功,running \x1b[38;2;39;184;219m${APP_HTTP}://${APP_HOST}:${APP_PORT}\x1b[0m`
   )
