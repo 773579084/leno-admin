b4b5cde58723fbe6983aae131bb40407d0e35d0a
zhaowenchao <773579084@qq.com>
2023-07-12 13:56:51 +0800

    ci:关闭默认的log文件写入，替换为pm2文件写入

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,8 +2,8 @@ module.exports = {
     apps: [{
         name: 'leno_admin_server',
         script: './dist/main.js',
-        error_file: './pm2logs/error.log', // 错误日志文件路径
-        out_file: './pm2logs/out.log', // 标准输出日志文件路径
+        error_file: './logs/error.log', // 错误日志文件路径
+        out_file: './logs/out.log', // 标准输出日志文件路径
         env: {
             NODE_ENV: 'production'
         }
