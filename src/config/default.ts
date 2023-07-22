ed24b59676f66fa2005f175dabd9ee12fe4a6d57
zhaowenchao <773579084@qq.com>
2023-07-22 15:04:37 +0800

    build:docker 项目容器化配置，docker compose多容器管理配置

--- src/config/default.ts
+++ src/config/default.ts
@@ -1,8 +1,6 @@
 export default () => {
   // 重要：需要 cross-env 注入变量启动，才能够全局使用，直接用node启动文件无法获取变量
   if (process.env.NODE_ENV === 'development') {
-    console.log(4, process.env.NODE_ENV)
-
     return {
       // 服务器运行地址及端口
       APP_HOST: 'localhost',
@@ -30,7 +28,7 @@ export default () => {
       APP_PORT: 9000,
       APP_HTTP: 'http',
       // mysql
-      MYSQL_HOST: 'localhost',
+      MYSQL_HOST: 'mysql',
       MYSQL_PORT: 3306,
       MYSQL_USER: 'root',
       MYSQL_PWD: 'Aa363689;',
@@ -40,7 +38,7 @@ export default () => {
       JWT_REFRESH_SECRET: 'refreshToken',
       // redis
       REDIS_PORT: 6379,
-      REDIS_HOST: 'localhost',
+      REDIS_HOST: 'redis',
       REDIS_PASSWORD: 'admin123',
       REDIS_DB: 0
     }
