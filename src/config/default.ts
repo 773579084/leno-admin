d2a7ba5fc5df73d8dbd96526174cb2a7d80a0bf1
zhaowenchao <773579084@qq.com>
2023-08-03 01:26:48 +0000

    chore:新增运行环境

--- src/config/default.ts
+++ src/config/default.ts
@@ -1,13 +1,14 @@
-export default () => {
+export default () => {  
   // 重要：需要 cross-env 注入变量启动，才能够全局使用，直接用node启动文件无法获取变量
   if (process.env.NODE_ENV === 'development') {
+    // windows和mac都是通过host.docker.internal连接宿主机
     return {
       // 服务器运行地址及端口
       APP_HOST: 'localhost',
       APP_PORT: 9090,
       APP_HTTP: 'http',
-      // mysql
-      MYSQL_HOST: 'localhost',
+      // mysql 
+      MYSQL_HOST: 'host.docker.internal',
       MYSQL_PORT: 3306,
       MYSQL_USER: 'root',
       MYSQL_PWD: 'admin123',
@@ -17,11 +18,33 @@ export default () => {
       JWT_REFRESH_SECRET: 'refreshToken',
       // redis
       REDIS_PORT: 6379,
-      REDIS_HOST: 'localhost',
+      REDIS_HOST: 'host.docker.internal',
       REDIS_PASSWORD: 'admin123',
       REDIS_DB: 0
     }
-  } else {
+  } else if(process.env.NODE_ENV === 'remote'){
+    return {
+      // 服务器运行地址及端口
+      APP_HOST: 'localhost',
+      APP_PORT: 9090,
+      APP_HTTP: 'http',
+      // mysql
+      MYSQL_HOST: '120.79.243.62',
+      MYSQL_PORT: 3306,
+      MYSQL_USER: 'root',
+      MYSQL_PWD: 'Aa363689;',
+      MYSQL_DB: 'leno_admin',
+      // JWT
+      JWT_SECRET: 'lenoAdmin',
+      JWT_REFRESH_SECRET: 'refreshToken',
+      // redis
+      REDIS_PORT: 6379,
+      REDIS_HOST: '120.79.243.62',
+      REDIS_PASSWORD: 'admin123',
+      REDIS_DB: 0
+    }
+  }
+   else if(process.env.NODE_ENV === 'production'){
     return {
       // 服务器运行地址及端口
       APP_HOST: 'localhost',
