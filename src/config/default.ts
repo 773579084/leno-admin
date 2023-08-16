22e56c0a5e20d7de0ddc3d86325434500616532c
zhaowenchao <773579084@qq.com>
2023-08-16 02:07:31 +0000

    chore:增加config及package配置项

--- src/config/default.ts
+++ src/config/default.ts
@@ -8,7 +8,7 @@ export default () => {
       APP_PORT: 9090,
       APP_HTTP: 'http',
       // mysql
-      MYSQL_HOST: 'localhost',
+      MYSQL_HOST: 'mysql',
       MYSQL_PORT: 3306,
       MYSQL_USER: 'root',
       MYSQL_PWD: 'admin123',
@@ -18,13 +18,38 @@ export default () => {
       JWT_REFRESH_SECRET: 'refreshToken',
       // redis
       REDIS_PORT: 6379,
-      REDIS_HOST: 'localhost',
+      REDIS_HOST: 'redis',
+      // REDIS_PASSWORD: 'admin123',
+      REDIS_DB: 0,
+      // 图片路径
+      IMG_URL: 'http://localhost:9090/static/'
+    }
+  }else if (process.env.NODE_ENV === 'docker-desktop') {
+    return {
+      // 服务器运行地址及端口
+      APP_HOST: 'localhost',
+      APP_PORT: 9090,
+      APP_HTTP: 'http',
+      // mysql
+      MYSQL_HOST: 'mysql',
+      MYSQL_PORT: 4000,
+      MYSQL_USER: 'root',
+      MYSQL_PWD: 'Aa363689;',
+      MYSQL_DB: 'leno_admin',
+      // JWT
+      JWT_SECRET: 'lenoAdmin',
+      JWT_REFRESH_SECRET: 'refreshToken',
+      // redis
+      REDIS_PORT: 4100,
+      // REDIS_HOST: 'host.docker.internal',
+      REDIS_HOST: 'host.docker.internal',
       REDIS_PASSWORD: 'admin123',
       REDIS_DB: 0,
       // 图片路径
       IMG_URL: 'http://localhost:9090/static/'
     }
-  } else if (process.env.NODE_ENV === 'docker') {
+  } 
+   else if (process.env.NODE_ENV === 'docker-local') {
     return {
       // 服务器运行地址及端口
       APP_HOST: 'localhost',
